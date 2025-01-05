7effa229a5f240222ac4683eb8861f87ec1dbc2d
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2025-01-05 09:08:37 +0000

    update runirpass.sh

--- src/intervalrangepass/test/runirpass.sh
+++ src/intervalrangepass/test/runirpass.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
 /tip/tipc/build/src/tipc -do $1.tip
 llvm-dis-14 $1.tip.bc
-opt-14 -enable-new-pm=0 -load /tip/tipc-passes/build/src/intervalrangepass/irpass.so -mem2reg -irpass < $1.tip.bc >/dev/null 2>$1.irpass
+cmake --build /tip/tipc-passes/build
+if [ "$#" -eq 2 ] && [ "$2" == "debug" ]; then
+    opt-14 -enable-new-pm=0 -load /tip/tipc-passes/build/src/intervalrangepass/irpass.so -intervalrange-debug -mem2reg -irpass < $1.tip.bc >/dev/null 2>$1.irpass
+else
+    opt-14 -enable-new-pm=0 -load /tip/tipc-passes/build/src/intervalrangepass/irpass.so -mem2reg -irpass < $1.tip.bc >/dev/null 2>$1.irpass
+fi
