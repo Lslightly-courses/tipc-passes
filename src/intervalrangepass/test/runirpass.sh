4f9432a6e57038e4e6b74fd4bc057a2acc98aa79
lqw@128C <lqw332664203@mail.ustc.edu.cn>
2025-01-05 05:33:11 +0000

    update runirpass

--- src/intervalrangepass/test/runirpass.sh
+++ src/intervalrangepass/test/runirpass.sh
@@ -1,3 +1,4 @@
-~/tipc/build/src/tipc -do $1.tip
-llvm-dis $1.tip.bc
-opt -enable-new-pm=0 -load ~/tipc-passes/build/src/intervalrangepass/irpass.dylib -mem2reg -irpass < $1.tip.bc >/dev/null 2>$1.irpass
+ROOT=/tip
+$ROOT/tipc/build/src/tipc -do $1.tip
+llvm-dis-14 $1.tip.bc
+opt-14 -enable-new-pm=0 -load $ROOT/tipc-passes/build/src/intervalrangepass/irpass.so -mem2reg -irpass < $1.tip.bc >/dev/null 2>$1.irpass
